aa0b6b038a168b3407236ea4b3d89fc17f82dbff
Tamara Glushchenko <gluschenkotoma@ukr.net>
2021-11-06 18:59:30 +0200

    todo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
 import React, { Component } from 'react';
+import shortid from 'shortid';
+
 import TodoList from './components/TodoList';
+import TodoAdd from './components/TodoAdd';
+import Filter from './components/Filter';
 
 import initialTodo from './todos.json';
 
 class App extends Component {
   state = {
     todos: initialTodo,
+    //   значение Filter
+    filter: '',
   };
+
+  // ДОБАВЛЕНИЕ ТУДУШКИ ============================================================
+  // получения текста с текстерии
+  // проп для формы
+  // в text приходит -> this.state.message c this.props.onSubmit(this.state.message); //<TodoAdd onSubmit={this.addTodo} />
+  addTodo = text => {
+    console.log(text); //hjjhvjhvjh
+    const todo = {
+      id: shortid.generate(),
+      // имя свойства, имя переменной
+      text: text,
+      completed: false,
+    };
+    //   добавить обьект todo  в new массив todos методом распыления, от предыдущего состояния, сразу кинуть то что создали, и следующее - то что было в предыдущей todos
+    this.setState(prevState => ({
+      todos: [todo, ...prevState.todos],
+    }));
+
+    // this.setState({todos} => ({
+    //   todos: [todo, ...todos],
+  };
+
+  // УДАЛЕНИЕ
   // filer - выкинуть элемент ид которого совпадают
   // вернуть те элементы ид которого не совпадают
   // в todoId будет заходить ид с анонимки onDeleteTodo(id)
@@ -16,22 +45,49 @@ class App extends Component {
     }));
   };
 
-  render() {
-    const { todos } = this.state;
-    const totalTodoCount = todos.length;
-    const completedTodoCount = todos.reduce(
-      (total, todo) => (todo.id ? total + 1 : total),
-      0,
+  // ФИЛЬТРАЦИЯ
+  //принятие ивента
+  changeFilter = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+  // метод фильтрации массива
+  getVisibleTodos = () => {
+    const { todos, filter } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+    return todos.filter(todo =>
+      todo.text.toLowerCase().includes(normalizedFilter),
     );
+  };
+  //
+  calculateCompletedTodo = () => {
+    const { todos } = this.state;
+    return todos.reduce((total, todo) => (todo.id ? total + 1 : total), 0);
+  };
+
+  render() {
+    const { todos, filter } = this.state;
+    const visibleTodos = this.getVisibleTodos();
+
+    const totalTodoCount = todos.length;
+    const completedTodoCount = this.calculateCompletedTodo();
+
+    //   для фильтра
+
     return (
       <>
+        {/* this.props.onSubmit(this.state.message); */}
+        <TodoAdd onSubmit={this.addTodo} />
+
+        {/* <TodoList todos={todos} onDeleteTodo={this.deleteTodo} /> */}
+        {/* рендер только видимых, отфильтрованых тудушек */}
+        <TodoList todos={visibleTodos} onDeleteTodo={this.deleteTodo} />
+        <Filter onChange={this.changeFilter} value={filter} />
         <h1>Phonebook</h1>
+
         <div>
           <p>Oбщее количество: {totalTodoCount}</p>
           <p>Kоличество выполненых: {completedTodoCount}</p>
         </div>
-
-        <TodoList todos={todos} onDeleteTodo={this.deleteTodo} />
       </>
     );
   }
--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -3,11 +3,11 @@ import s from './TodoList.module.scss';
 //                  передача пропсов с <TodoList todos={todos} onDeleteTodo={this.deleteTodo} />
 const TodoList = ({ todos, onDeleteTodo }) => (
   <ul className={s.list}>
-    {/*  id, name, number      деструкт-е ключи с масива обьектов */}
-    {todos.map(({ id, name, number }) => (
+    {/*  id, text, completed     деструкт-е ключи с масива обьектов */}
+    {todos.map(({ id, text, completed }) => (
       <li key={id} className={s.list__item}>
         <p className={s.list__text}>
-          {name} <span>{number}</span>
+          {text} <span>{completed}</span>
         </p>
         <button type="button" onClick={() => onDeleteTodo(id)}>
           Delete
